c6a688d0a1c9d24733dbdce5f28a9116ff705471
Iván Raskovsky <raskovsky+git@gmail.com>
2015-11-25 18:08:12 -0300

    concatenar y super sub indice

--- parser.py
+++ parser.py
@@ -1,9 +1,76 @@
-
 # -----------------------------------------------------------------------------
 # parser.py
 #
 # A simple calculator with variables -- all in one file.
 # -----------------------------------------------------------------------------
+LAMBDA = ''
+
+class Nodo(object):
+    texto = None
+    ancho = None
+    alto = None
+
+    def __init__(self, texto='', ancho=0, alto=0):
+        self.texto = texto
+        self.ancho = ancho
+        self.alto = alto
+
+    def __str__(self):
+        return self.texto
+
+    def __bool__(self):
+        return self.texto != ''
+
+    def concatenar(self, nodo):
+        if nodo.texto == '':
+            return self
+        
+        return Nodo('''
+            %s
+            <g transform="translate(%s, 0)">
+            %s
+            </g>
+        ''' % (self.texto, self.ancho, nodo.texto),
+        self.ancho + nodo.ancho,
+        self.alto + nodo.alto)
+
+    def superponer(self, nodo):
+        return Nodo('''
+        %s
+        %s
+        ''' % (self.texto, nodo.texto),
+        max(self.ancho, nodo.ancho),
+        self.ancho + nodo.alto)
+
+    def subir(self):
+        nodo = self.a_indice()
+        return Nodo('''
+        <g transform="translate(0, -3)">
+            %s
+        </g>
+        ''' % nodo.texto,
+        nodo.ancho,
+        nodo.alto)
+
+    def bajar(self):
+        nodo = self.a_indice()
+        return Nodo('''
+        <g transform="translate(0, 3)">
+            %s
+        </g>
+        ''' % nodo.texto,
+        nodo.ancho,
+        nodo.alto)
+
+    def a_indice(self):
+        return Nodo('''
+        <g transform="scale(.7)">
+            %s
+        </g>
+        ''' % self.texto,
+        self.ancho * 0.7,
+        self.alto * 0.7)
+
 
 tokens = (
     'CARACTER', 'DIVISION', 'SUB', 'SUPER', 'PARENIZQ', 'PARENDER',
@@ -49,9 +116,9 @@ def p_statement_expr(t):
     out.write('''<?xml version="1.0" standalone="no"?>
         <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
         <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
-            <g transform="translate(0, 200) scale(200)" font-family="Courier">
+            <g transform="translate(0, 200) scale(10)" font-family="Courier" font-size="10">
     ''')
-    out.write(t[1])
+    out.write(t[1].texto)
     out.write('''
         </g>
     </svg>
@@ -61,17 +128,20 @@ def p_statement_expr(t):
 def p_term(t):
     '''term : factor factor1
     '''
-    t[0] = "%s%s" % (t[1], t[2])
+    t[0] = t[1].concatenar(t[2])
 
 def p_factor1(t):
     '''factor1 : lambda
                | SUPER factor factor2
                | SUB factor factor3
     '''
-    if t[1] != 'lambda':
-        t[0] = ' %s %s%s' % (t[1], t[2], t[3])
+    if t[1] != LAMBDA:
+        if t[1] == '^':
+            t[0] = t[2].subir().superponer(t[3])
+        elif t[1] == '_':
+            t[0] = t[2].bajar().superponer(t[3])
     else:
-        t[0] = ''
+        t[0] = Nodo('')
 
 def p_factor2(t):
     '''factor2 : lambda
@@ -79,53 +149,56 @@ def p_factor2(t):
        factor3 : lambda
                | SUPER factor
     '''
-    if t[1] != 'lambda':
-        t[0] = ' %s %s' % (t[1], t[2])
+    if t[1] != LAMBDA:
+        if t[1] == '^':
+            t[0] = t[2].subir()
+        elif t[1] == '_':
+            t[0] = t[2].bajar()
     else:
-        t[0] = ''
+        t[0] = Nodo('')
 
 def p_expression(t):
     '''expression : term1 term
     '''
-    t[0] = "%s%s" % (t[1], t[2])
+    t[0] = t[1].concatenar(t[2])
 
 def p_term1(t):
     '''term1 : lambda
             | term1 term
     '''
-    if t[1] != 'lambda':
-        t[0] = ' %s%s' % (t[1], t[2])
+    if t[1] != LAMBDA:
+        t[0] = t[1].concatenar(t[2])
     else:
-        t[0] = ''
+        t[0] = Nodo()
 
 def p_div(t):
     '''div : div1 expression
     '''
-    t[0] = "%s%s" % (t[1], t[2])
+    t[0] = Nodo("%s%s" % (t[1], t[2]), max(t[1].ancho, t[2].ancho, 100))
 
 def p_div1(t):
     '''div1 : lambda
             | div1 expression DIVISION
     '''
-    if t[1] != 'lambda':
-        t[0] = ' %s%s /' % (t[1], t[2])
+    if t[1] != LAMBDA:
+        t[0] = Nodo(' %s%s /' % (t[1], t[2]), max(t[1].ancho, t[2].ancho, 100))
     else:
-        t[0] = ''
+        t[0] = Nodo('')
 
 def p_group(t):
     '''factor : PARENIZQ div PARENDER'''
-    t[0] = '%s' % t[2]
+    t[0] = t[2]
 
 def p_expression_caracter(t):
     'factor : CARACTER'
-    t[0] = t[1]
+    t[0] = Nodo('''<text>%s</text>''' % t[1], 6, 10)
 
 def p_error(t):
     print("Syntax error at '%s'" % t.value)
 
 def p_lambda(t):
-    'lambda :'
-    t[0] = 'lambda'
+    '''lambda :'''
+    t[0] = LAMBDA
 
 import ply.yacc as yacc
 parser = yacc.yacc()
