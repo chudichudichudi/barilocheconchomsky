1af30f7e4fb13420b83b5630bb34722e98862329
Iván Raskovsky <raskovsky+git@gmail.com>
2015-12-09 20:38:37 -0300

    mas tests y arregladas algunas cosas

--- run_tests.py
+++ run_tests.py
@@ -1,14 +1,16 @@
 from tlen_tp import parser, ESCALA
 
 formulas = [
-    "(C / {A + {D/E}} )-I",
-    "({A + {D/E}} / C)-I",
-    "(A^B_DC^D/E^F_G+H)-I",
-    "(A_{D_{D_{D_{D_{D_{D_{D}}}}}}}^{D^{D^{D^{D^{D^{D}}}}}})",
-    "(D)",
-    "(A/B/C/D)",
-    "(E^E_E)",
-    "(E_E^E)",
+    ("C", "Un sólo caracter"),
+    ("(C)", "Un sólo caracter entre paréntesis"),
+    ("(E^E_E)", "Alineación de super sub índice (1)"),
+    ("(E_E^E)", "Alineación de super sub índice (2)"),
+    ("(A_{D_{D_{D_{D_{D_{D_{D}}}}}}}^{D^{D^{D^{D^{D^{D}}}}}})", "Alineación de super sub índice (3)"),
+    ("(C / {A + {D/E}} )-I", ""),
+    ("({A + {D/E}} / C)-I", ""),
+    ("(A^B_DC^D/E^F_G+H)-I", ""),
+    ("(D)", ""),
+    ("(A/B/C/D)", ""),
 ]
 
 template = """
@@ -24,6 +26,9 @@ template = """
         width: 100%;
         height: auto;
     }}
+    svg {{
+        border: 1px solid #000;
+    }}
     </style>
 </head>
 <body>
@@ -37,11 +42,12 @@ template = """
 """
 
 render = ''
-for formula in formulas:
+for formula, descripcion in formulas:
     out = parser.parse(formula)
     render += """
     <li>
-        <p>Fórmula: {formula}</p>
+        <h3>Fórmula: {formula}</h3>
+        <p>{descripcion}</p>
         <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="{ANCHO_TOTAL}" height="{ALTO_TOTAL}" viewbox="0 0 {ANCHO_TOTAL} {ALTO_TOTAL}">
             <g transform="translate(0, {ALTO_TOTAL}) scale({ESCALA})" font-family="Courier" font-size="10">
                 {render}
@@ -50,6 +56,7 @@ for formula in formulas:
     </li>
     """.format(
         formula=formula,
+        descripcion=descripcion,
         render=out,
         ALTO_TOTAL=(abs(out.alto_arriba) +  abs(out.alto_abajo) ) * ESCALA,
         ANCHO_TOTAL=out.ancho * ESCALA,
